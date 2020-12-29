2a1e1c0841504f8b6681491abf7989c3d98c2641
Thiago Pinheiro <dev.thiagopinheiro@gmail.com>
2020-12-29 20:22:30 -0300

    Componentização

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,14 @@
+import React from 'react'
+import Header from './components/Header'
 
 function App() {
   return (
-    <h1>Olá mundo</h1>
-  );
+    <>
+      <Header/>
+      <Header/>
+      <Header/>
+    </>
+  )
 }
 
 export default App;
