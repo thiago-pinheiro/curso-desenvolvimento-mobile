91b3173cafbfc1788c80cfa88af23eb41d8acc98
Thiago Pinheiro <dev.thiagopinheiro@gmail.com>
2021-01-03 14:53:23 -0300

    Estado e Imutabilidade

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Header from './components/Header'
 
 function App() {
+
+  const [projects, setProjects] = useState(["Facebook", "Uber"])
+
+  function handleAddProject(){ 
+   
+    setProjects([ ...projects, 'Projeto criado em: ${Date.now()}'])
+    
+  }
+
+
   return (
     <>
       <Header title="React">
         <ul>
-          <li>Componentização</li>
-          <li>Propriedades</li>
+          {projects.map(project => (
+            <li key={project}>
+              {project}
+            </li>
+          ))}
         </ul>
       </Header>
+
+      <button type="button" onClick={handleAddProject}>
+        Adicionar Projeto
+      </button>
 
     </>
   )
